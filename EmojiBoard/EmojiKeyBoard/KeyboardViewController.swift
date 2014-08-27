a16e70b330bea0f482ac04ed437c2ecfb9db743a
Jim Stewart <jim@stewartstuff.com>
2014-08-26 22:19:27 -0600

    Add dot button for validation

--- EmojiBoard/EmojiKeyBoard/KeyboardViewController.swift
+++ EmojiBoard/EmojiKeyBoard/KeyboardViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class KeyboardViewController: UIInputViewController {
 
     @IBOutlet weak var nextKeyboardButton: UIButton!
+    
+    var dotButton: UIButton!
 
     override func updateViewConstraints() {
         super.updateViewConstraints()
@@ -42,6 +44,42 @@ class KeyboardViewController: UIInputViewController {
     
     func addKeyboardButtons() {
         addNextKeyboardButton()
+        addDot()
+    }
+    
+    
+    func addDot() {
+        // initialize the button
+        dotButton = UIButton.buttonWithType(.System) as UIButton
+        dotButton.setTitle(".", forState: .Normal)
+        dotButton.sizeToFit()
+        dotButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        // adding a callback
+        dotButton.addTarget(self, action: "didTapDot", forControlEvents: .TouchUpInside)
+        
+        // make the font bigger
+        dotButton.titleLabel.font = UIFont.systemFontOfSize(32)
+        
+        // add rounded corners
+        dotButton.backgroundColor = UIColor(white: 0.9, alpha: 1)
+        dotButton.layer.cornerRadius = 5
+        
+        view.addSubview(dotButton)
+        
+        // makes the vertical centers equa;
+        var dotCenterYConstraint = NSLayoutConstraint(item: dotButton, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1.0, constant: 0)
+        
+        // set the button 50 points to the left (-) of the horizontal center
+        var dotCenterXConstraint = NSLayoutConstraint(item: dotButton, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: -50)
+        
+        view.addConstraints([dotCenterXConstraint, dotCenterYConstraint])
+    }
+    
+    func didTapDot() {
+        var proxy = textDocumentProxy as UITextDocumentProxy
+        
+        proxy.insertText(".")
     }
 
     override func didReceiveMemoryWarning() {
